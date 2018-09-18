254ffef0ca0bffd10b11107b28003e8200d42a1c
Peterson <contact@peterson.by>
2018-09-18 13:57:05 +0300

    brackets check implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2 !== 0) return false;
+  
+  const brackets = bracketsConfig.map(brackets => brackets.join(''));
+
+  let newStr = '';
+
+  while (newStr.length !== str.length) {
+    newStr = str;
+	  
+	for (let i = 0; i < brackets.length; i++) {
+	  str = str.replace(brackets[i], '');
+	}
+  }
+
+  return str.length ? false : true;
 }
